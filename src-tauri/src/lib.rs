83853b7e792117af73c8fee5d827bc00203c97a8
katrate <katrate@users.noreply.github.com>
2026-05-30 23:24:43 +0530

    fix: remove updater signing, builds will succeed now

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -51,7 +51,6 @@ pub fn run() {
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_process::init())
-        .plugin(tauri_plugin_updater::Builder::new().build())
         .invoke_handler(tauri::generate_handler![attach_file, file_exists])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
